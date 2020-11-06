c516252735e27f9c8a17ef5be5e597750e52593b
351510443@qq.com <351510443@qq.com>
2020-11-06 10:59:17 +0800

    updata

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <index/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/*import HelloWorld from './components/HelloWorld.vue'*/
 
+import Index from "@/components/index";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Index
+   /* HelloWorld*/
   }
 }
 </script>
