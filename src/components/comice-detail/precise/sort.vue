276826582fa69a9eccad85f946db2b44b8e3c490
351510443@qq.com <351510443@qq.com>
2020-11-07 22:18:57 +0800

    updata

--- src/components/comice-detail/precise/sort.vue
+++ src/components/comice-detail/precise/sort.vue
@@ -1,9 +1,10 @@
-
 <!--分类页面-->
 <template>
-<div>
-  {{list[0]}}
-</div>
+  <div class="sort-parent">
+    <div v-for="item in list" v-bind:key="item.tag_id" class="sort-img">
+      <img :src="item.cover">
+    </div>
+    </div>
 </template>
 
 <script>
@@ -11,25 +12,39 @@ import http from "@/components/utils/http";
 
 export default {
   name: "sort",
-  data(){
-    return{
-      list:[]
+  data() {
+    return {
+      list: []
     }
-  },created() {
+  }, created() {
     this.getData();
   },
-  methods:{
-    getData(){
+  methods: {
+    getData() {
       let url = "/0/category.json?terminal_model=MI%20MAX%203&channel=Android&_debug=0&imei=3264861218cb65b7&version=2.7.035&timestamp=1604753855";
-      http.get(url,params=>{
+      http.get(url, params => {
         this.list = params;
         console.log("params: " + this.list)
       })
     }
-}
+  }
 }
 </script>
 
 <style scoped>
+.sort-parent{
+  display: flex;
+  flex-direction:row;
+  flex-wrap:wrap;
+}
+.sort-img{
+  width: 30%;
+  height: 30%;
+  margin: 5.5px;
+}
+img{
+ width: 100%;
+  height: 100%;
 
+}
 </style>
